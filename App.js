c1afb4ca0cf386939fe060375af24b6df03b7c15
vjaxel34 <92274553+vjaxel34@users.noreply.github.com>
2022-03-09 22:20:41 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import BasicInfo from './component/BasicInfo'
+// Very Easy Challenge to Easy 
+// class App extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       person: "Axel"
+//     }
+//   }
+//   render(){
+//     return(
+//       <div className="App">
+//         <p>
+//           Axel, 21, 01/23/2001
+//         </p>
+//       </div>
+//     )
+//   }
+// }
+class app extends Component{
+  constructor(props){
+    this.state.person
+  }
+  render(){
+    return(
+      <div className="App">
+        <p>
+          Axel, 21, 01/23/2001
+        </p>
+      </div>
+    )
+  }
+ }
+export default App
+
+//
---
+++ BasicInfo.js
@@ -0,0 +1,11 @@
+class BasicInfo {
+    constructor(props) {
+      this.name = this.name;
+      this.dob = this.dob;
+      this.age = this.age;
+    }
+  }
+  
+
+  //
+  
